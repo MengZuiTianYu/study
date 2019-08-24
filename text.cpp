e8e1901507813330dd43dde8f5aab2d6331f26f5
XinYuan Wang <xywang@aiit.org.cn>
2019-08-24 21:45:18 +0800

    dw[C

--- text.cpp
+++ text.cpp
@@ -3,4 +3,29 @@ int main()
 {
     std::cout << "hello" << std::endl;
     std::cout << "hello" << std::endl;
+    
+    int i;
+    std::cout << "i++:"<<std::endl;
+    for(i=0; i<5; i++) {
+        std::cout << "in -- i:" << i<<std::endl;
+    }
+    std::cout << "out -- i:" << i<<std::endl;
+
+    std::cout << "++i:"<<std::endl;
+    for(i=0; i<5; ++i) {
+        std::cout << "in -- i:" << i<<std::endl;
+    }
+    std::cout << "out -- i:" << i<<std::endl;
+
+    std::cout << "inside i++:"<<std::endl;
+    for(i=0; i<5;) {
+        std::cout << "in -- i:" << i++<<std::endl;
+    }
+    std::cout << "out -- i:" << i<<std::endl;
+
+    std::cout << "inside ++i:"<<std::endl;
+    for(i=0; i<5;) {
+        std::cout << "in -- i:" << ++i<<std::endl;
+    }
+    std::cout << "out -- i:" << i<<std::endl;
 }
